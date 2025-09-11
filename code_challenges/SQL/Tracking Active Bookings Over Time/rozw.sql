30d32f1b00ad79b18981e22483bf9eb57704cfa0
Loe-Aner <146349544+Loe-Aner@users.noreply.github.com>
2025-09-11 18:23:19 +0200

    Dodano nowe code challenges_SQL

---
+++ code_challenges/SQL/Tracking Active Bookings Over Time/rozw.sql
@@ -0,0 +1,20 @@
+WITH course_bookings AS (
+  SELECT *
+  FROM read_csv('D:\MyProjects_4Fun\code_challenges\SQL\Tracking Active Bookings Over Time\sample.csv')
+),
+
+dts AS (
+SELECT DISTINCT booking_date
+FROM course_bookings
+)
+
+SELECT
+  d.booking_date,
+  COUNT(*) AS active_bookings
+FROM dts AS d
+JOIN course_bookings AS b
+  ON b.booking_date <= d.booking_date
+ AND b.course_start_date >= d.booking_date
+GROUP BY d.booking_date
+ORDER BY d.booking_date;
+
